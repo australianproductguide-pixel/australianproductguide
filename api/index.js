6a43db8e6e79c9d7cc94bd715c86d281ccf4be6f
australianproductguide-pixel <smartbuydirectau@gmail.com>
2026-08-17 21:01:40 +1000

    Route APG through Research View v43.1 polish

--- api/index.js
+++ api/index.js
@@ -1,2 +1,2 @@
-// APG Research View v43 over priority commerce depth v42.
-module.exports=require('../lib/research-view-v43');
+// APG Research View v43.1 over priority commerce depth v42.
+module.exports=require('../lib/research-view-v431');
