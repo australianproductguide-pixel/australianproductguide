8f0872fe80ad5d90a005517b1eef543ad49b0ae8
australianproductguide-pixel <contact@australianproductguide.au>
2026-08-22 18:51:52 +1000

    Add curated vector overrides for grainy brand marks

---
+++ data/brand-mark-curated-overrides-v66.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// APG Brand Mark Curated Overrides v66
+// Reviewed vector identity assets for brands whose official-site auto-discovery can
+// resolve to a low-resolution raster, grey favicon, or no suitable public mark.
+// These assets are used only for nominative brand identification. They do not imply
+// sponsorship, endorsement, partnership or ownership of the underlying trademarks.
+// The corresponding official brand site remains the identity/source-of-truth reference.
+module.exports=Object.freeze({
+  breville:Object.freeze({
+    brand:'Breville',
+    assetUrl:'https://upload.wikimedia.org/wikipedia/commons/6/6c/Breville_logo.svg',
+    sourcePage:'https://commons.wikimedia.org/wiki/File:Breville_logo.svg',
+    officialReference:'https://www.breville.com/en-au',
+    format:'svg',
+    reviewStatus:'curated-reviewed-vector'
+  }),
+  samsung:Object.freeze({
+    brand:'Samsung',
+    assetUrl:'https://upload.wikimedia.org/wikipedia/commons/b/b4/Samsung_wordmark.svg',
+    sourcePage:'https://commons.wikimedia.org/wiki/File:Samsung_wordmark.svg',
+    officialReference:'https://www.samsung.com/au/',
+    format:'svg',
+    reviewStatus:'curated-reviewed-vector'
+  }),
+  philips:Object.freeze({
+    brand:'Philips',
+    assetUrl:'https://upload.wikimedia.org/wikipedia/commons/8/8c/Philips_logo.svg',
+    sourcePage:'https://commons.wikimedia.org/wiki/File:Philips_logo.svg',
+    officialReference:'https://www.philips.com.au/',
+    format:'svg',
+    reviewStatus:'curated-reviewed-vector'
+  }),
+  delonghi:Object.freeze({
+    brand:"De'Longhi",
+    assetUrl:'https://upload.wikimedia.org/wikipedia/commons/d/de/De%E2%80%99Longhi.svg',
+    sourcePage:'https://commons.wikimedia.org/wiki/File:De%E2%80%99Longhi.svg',
+    officialReference:'https://www.delonghi.com/en-au',
+    format:'svg',
+    reviewStatus:'curated-reviewed-vector'
+  }),
+  dyson:Object.freeze({
+    brand:'Dyson',
+    assetUrl:'https://upload.wikimedia.org/wikipedia/commons/2/2f/Dyson_logo.svg',
+    sourcePage:'https://commons.wikimedia.org/wiki/File:Dyson_logo.svg',
+    officialReference:'https://www.dyson.com.au/',
+    format:'svg',
+    reviewStatus:'curated-reviewed-vector'
+  })
+});
