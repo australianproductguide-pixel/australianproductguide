a45a657b8038b4454592732142e798f9b1160a6f
australianproductguide-pixel <contact@australianproductguide.au>
2026-09-06 13:07:18 +1000

    Redact Speed Insights URL query data

--- public/assets/vercel-speed-insights-v1.js
+++ public/assets/vercel-speed-insights-v1.js
@@ -1,2 +1,13 @@
 'use strict';
 window.si=window.si||function(){(window.siq=window.siq||[]).push(arguments)};
+window.si('beforeSend',function(event){
+  try{
+    var url=new URL(event&&event.url?event.url:location.href,location.origin);
+    if(url.pathname==='/my-apg'||url.pathname==='/my-apg/'||url.pathname.indexOf('/my-apg/')===0)return null;
+    url.search='';
+    url.hash='';
+    return Object.assign({},event,{url:url.origin+url.pathname});
+  }catch(_error){
+    return event;
+  }
+});
